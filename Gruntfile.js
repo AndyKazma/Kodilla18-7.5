138ea1149b6bd295545140f5a1df3b5cdc21ab5f
slavek.d <slavek@123321.pl>
2016-08-03 08:40:48 +0200

    pliki do grunt po dodaniu tasks

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass'
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    }
+    watch: {
+        scripts: {
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        } 
+    }
+
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+};
